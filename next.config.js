b53090de325bc7beda42689fe8297c54cdf8b2b5
danipatko <p8.pada@gmail.com>
2022-02-26 12:12:40 +0100

    create base config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    swcMinify: false, // true for production
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
