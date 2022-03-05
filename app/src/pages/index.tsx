8c5fef3a647270996302c04c3fc6643ecfebf927
danipatko <p8.pada@gmail.com>
2022-03-05 16:26:21 +0100

    modsec for nginx

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import Deadline from '../lib/components/shared/Deadline';
 import Layout from '../lib/components/home/Layout';
 
-const redirect = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/userinfo.email&include_granted_scopes=true&response_type=code&redirect_uri=http%3A//localhost%3A3000/api/auth/redirect&client_id=${CLIENT_ID}`;
+const redirect = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/userinfo.email%20https%3A//www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&response_type=code&redirect_uri=http%3A//localhost%3A3000/api/auth/redirect&client_id=${CLIENT_ID}`;
 
 const Home = () => {
     return (
