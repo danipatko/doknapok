8726a9f2d72c926d0cca3e12c6c023514c1d2745
botragyi.gergo@gmail.com <botragyi.gergo@gmail.com>
2022-03-06 14:00:34 +0100

    added account change to user frontend

--- src/lib/components/user/Layout.tsx
+++ src/lib/components/user/Layout.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import { getUser } from '../../server/google-api/token';
 import { redirectToRoot } from '../../server/types';
 import Image from 'next/image';
+import { CLIENT_ID } from '../../../lib/server/env';
+
+const redirect = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/userinfo.email%20https%3A//www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&response_type=code&redirect_uri=http%3A//localhost%3A3000/api/auth/redirect&client_id=${CLIENT_ID}`;
 
 const Navbar = ({
     onDarkmode,
@@ -21,7 +24,9 @@ const Navbar = ({
             <div className='flex justify-center items-center gap-2'>
                 <div className='hidden md:block'>{userdata.name}</div>
                 <div className='hidden md:block'>{userdata.class}</div>
-                <Image src={userdata.picture} width='30px' height='30px' className='rounded-full' />
+                <a href={redirect}>
+                    <Image src={userdata.picture} width='30px' height='30px' className='rounded-full' />
+                </a>
             </div>
         </div>
     );
